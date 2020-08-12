dc2d7b43f2a26d4da9f012f29ef94fe410a4c7ce
GTian4MSM <jiantingmsm@gmail.com>
2020-08-11 19:08:46 -0700

    Issue#7 Create Status Endpoint

---
+++ express-LocalLibrary/routes/status.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET Status. */
+router.get('/', function(req, res, next) {
+    res.render('status', { status: 'Online'});
+  });
+
+module.exports = router;
+
+/* 
+Coded by Jianting
+*/
