5dd3ab2c7cb1b86d0ef0403873f0be6b6d1fe066
wetwc198892 <103273791@qq.com>
2020-09-04 23:59:37 -0700

    DB connection

--- express-LocalLibrary/routes/status.js
+++ express-LocalLibrary/routes/status.js
@@ -2,13 +2,7 @@ var express = require('express');
 var mongoose = require('mongoose');
 var router = express.Router();
 
-// mongoose.connect('mongodb+srv://root:P@ssw0rd@library.b54a2.mongodb.net/Library?retryWrites=true&w=majority', (error) => {
-//   if (!error) {
-//     console.log("Success");
-//   } else {
-//     console.log("Error connecting to database");
-//   }
-// });
+mongoose.connect('mongodb+srv://root:P@ssw0rd@library.b54a2.mongodb.net/Library?retryWrites=true&w=majority', { useNewUrlParser: true, useCreateIndex: true });
 
 /* GET Status. */
 router.get('/', function (req, res, next) {
@@ -18,8 +12,12 @@ router.get('/', function (req, res, next) {
 });
 
 /* GET MongoDB connection status and render the status page */
-mongoose.connection.on("connected", function () {
-  console.log("DB connection OK");
+mongoose.connection.on("connected", function (err) {
+  if (err) {
+    console.log("DB connection Error");
+  } else {
+    console.log("DB connection OK");
+  }
   router.get('/', function (req, res, next) {
     res.render('status', { DBstatus: 'Online' })
   });
--- mongoose/connection.js
+++ mongoose/connection.js
@@ -3,8 +3,8 @@ var mongoose = require('mongoose');
 // Connect to mongoDB
 mongoose.connect('mongodb://localhost/devtest');
 
-mongoose.connection.once('open', function(){
+mongoose.connection.once('open', function () {
   console.log('Connection has been made.');
-}).on('error', function(error){
+}).on('error', function (error) {
   console.log('Connection error:', error);
 });
